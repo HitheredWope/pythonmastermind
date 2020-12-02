fad8f449adc3f7c91072c4974731ec3d23ec1f0b
Josh <jpdanielskemp@gmail.com>
2020-12-02 17:19:54 +0000

    Basic Display Prototype

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello Josh!!")
+maxRound = 8
+
+def start():
+    '''Runs before the game starts'''
+
+def roundDisplay(result, roundNum, code):
+    print("Round          " + result[0] + " " + result[1])
+    print("No. " + str(roundNum) + "   " + code)
+    print("  " + str(maxRound) + "            " + result[2] + " " + result[3])
+
+#result = ["W", "W", "R", " "]
+#roundNum = 1
+#code = "BGMO"
