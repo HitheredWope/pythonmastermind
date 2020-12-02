9cf8e97e62e184fb77088a4465dbd911ed6e86a1
HitheredWope <75383654+HitheredWope@users.noreply.github.com>
2020-12-02 17:20:52 +0000

    Welcome message

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("hello Josh!!")
+print("Welcome to Mastermind") 
+response = input("Press any button to start, or Q to quit: ")
+if(response=="Q" or response=="q"):
+    quit()
+else:
+    print("GAME")
